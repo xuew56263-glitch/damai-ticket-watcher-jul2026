1e7b6c1abe456948f6b462ad4f59d52a1a3f89c5
xuew56263-glitch <xuew56263@gmail.com>
2026-07-11 17:45:02 +0800

    Extend deadline and publish live check status

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'damai-monitor-v2';
+const CACHE = 'damai-monitor-v3';
 const ASSETS = ['./', './index.html', './app.js', './styles.css', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
